ce7f3fd72883f72473799ff51239ada9ba1c8318
Your Name <you@example.com>
2023-11-02 17:08:03 +0900

    init commit

---
+++ src/day1/HelloWorld.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println(1234);
+		System.out.print(1234);
+		System.out.println("Hello World");
+		
+	}
+
+}
